94f4d7a8519a044ba0d59bc2d6eae690be8388f5
pepsi-dsf <1204182164@qq.com>
2021-10-04 23:10:55 +0800

    chore:添加ts类型定义

---
+++ src/screens/project-list/list.tsx
@@ -0,0 +1,40 @@
+import { User } from "./search-panel";
+interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  pin: boolean;
+  organization: string;
+}
+
+interface ListProps {
+  list: Project[];
+  users: User[];
+}
+
+export const List = ({ list, users }: ListProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            {/* 这个?很关键.直接find很容易出现undefined 如果undefined.name则会报错
+                而如果有? 则直接返回的时undefined
+            */}
+            <td>
+              {users.find((user) => user.id === project.personId)?.name ||
+                "未知"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,31 +1,34 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // 0
+    //@ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      //@ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 //防抖函数，避免在input框中每次输入都引起param的变换而导致网络请求被频繁发送
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
+
   useEffect(() => {
     const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // React 会在执行当前 effect 之前对上一个 effect 进行清除
     return () => clearTimeout(timeout);
   }, [value, delay]);
   return debouncedValue;
