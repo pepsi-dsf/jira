28f1efdac66258e35eb38e327953355835ddc316
pepsi-dsf <1204182164@qq.com>
2021-10-18 18:24:39 +0800

    feat：用flex和grid布局页面

--- src/authenticated-app/index.tsx
+++ src/authenticated-app/index.tsx
@@ -1,12 +1,45 @@
+import styled from "@emotion/styled";
 import { useAuth } from "../context/auth-context";
 import { ProjectListScreen } from "../screens/project-list";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
-
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <HeaderItem>Logo</HeaderItem>
+          <HeaderItem>项目</HeaderItem>
+          <HeaderItem>用户</HeaderItem>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+// grid-area 用来给grid子元素起名字
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+const Main = styled.main``;
