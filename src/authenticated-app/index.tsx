e2a9ef08b91acf34a273d3fae56519f53eadaeb4
pepsi-dsf <1204182164@qq.com>
2021-10-18 18:40:19 +0800

    style:用Row布局页面

--- src/authenticated-app/index.tsx
+++ src/authenticated-app/index.tsx
@@ -1,15 +1,16 @@
 import styled from "@emotion/styled";
 import { useAuth } from "../context/auth-context";
 import { ProjectListScreen } from "../screens/project-list";
+import { Row } from "../components/lib";
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <HeaderItem>Logo</HeaderItem>
-          <HeaderItem>项目</HeaderItem>
-          <HeaderItem>用户</HeaderItem>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
@@ -22,24 +23,13 @@ export const AuthenticatedApp = () => {
   );
 };
 
-const HeaderItem = styled.h3`
-  margin-right: 3rem;
-`;
 const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem 1fr 6rem;
   height: 100vh;
 `;
 // grid-area 用来给grid子元素起名字
-const Header = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div``;
 const Main = styled.main``;
