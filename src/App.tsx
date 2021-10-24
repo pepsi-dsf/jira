68b2a101c05e3a7b77801f0b21494e467f7889ce
pepsi-dsf <1204182164@qq.com>
2021-10-24 11:07:15 +0800

    feat:添加errorboundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useAuth } from "./context/auth-context";
 import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticateApp } from "./unauthenticated-app";
+import { ErrorBoundary } from "./components/error-boundary";
 import "./App.css";
+import { FullPageErrorFallback } from "components/lib";
 function App() {
   const { user } = useAuth();
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticateApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticateApp />}
+      </ErrorBoundary>
     </div>
   );
 }
